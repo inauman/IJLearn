936b47e2b32b9290620e9574c636e0d05f7c4b28
Mohammad Nauman <md.nauman@gmail.com>
2023-02-05 14:28:30 -0700

    added greet method to main

--- src/main/java/com/nauman/ij/Main.java
+++ src/main/java/com/nauman/ij/Main.java
@@ -4,4 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    public void greet(){
+        System.out.println("Hello Word!");
+    }
 }
