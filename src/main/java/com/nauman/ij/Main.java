e0bf89c1fa84257ede753fd98ccabf9d36f40dd0
Mohammad Nauman <md.nauman@gmail.com>
2023-02-05 15:22:40 -0700

    Greet, Log method enhancements and 2 bug fixes (#1)
    Enhanced greeting + printing + bugging :)

--- src/main/java/com/nauman/ij/Main.java
+++ src/main/java/com/nauman/ij/Main.java
@@ -4,4 +4,13 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    public void greet(String name){
+        System.out.println("Hello " + name + "!");
+    }
+
+    public void log(String logMsg){
+        //inc002: fixed log message.
+        System.out.println(logMsg);
+    }
 }
